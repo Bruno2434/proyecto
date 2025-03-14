a341dfb0a64747c20c31ff9224b972431cd49912
nite <brunomingorance134@gmail.com>
2025-03-14 13:04:15 -0300

    agregamos html y modificacion de viws

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Question
+from django.template import loader
 # Create your views here.
 
 def index(request):
-    return HttpResponse('hola mundo')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse('Estas viendo la pregunta %s' % question_id)
