6657ccd448662318b93b5a08dc4515ad499888c1
nite <brunomingorance134@gmail.com>
2025-03-13 23:14:52 -0300

    cam

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('hola mundo')
+
+def detail(request, question_id):
+    return HttpReponse('Estas viendo la pregunta %s' % question_id)
+
+def results(request, question_id):
+    response = 'Estas viendo la respuesta para la pregunta %s'
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse('Estas viendo las votaciones para la pregunta %s' % question_id)
